23ae53f5d3bca66a2b42057725bed830650dd516
Jorik van Rijn <jorik.vanrijn@gmail.com>
2020-09-05 00:32:22 +0200

    slightly improved invasioncounter.py

--- InvasionCounter_v2.py
+++ InvasionCounter_v2.py
@@ -2,6 +2,7 @@ import ij.IJ as IJ
 import ij.ImagePlus as ImagePlus
 import ij.measure.ResultsTable as ResultsTable
 import ij.measure.Measurements as Measurements
+import ij.io.Opener as Opener
 import ij.plugin.ChannelSplitter as ChannelSplitter
 import ij.plugin.HyperStackConverter as HyperStackConverter
 import ij.plugin.ZProjector as ZProjector
@@ -92,58 +93,63 @@ def main():
     # Prepare directory tree for output.
     indir = IJ.getDirectory("input directory")
     outdir = IJ.getDirectory(".csv output directory")
-    dapidir = os.path.join(outdir, "dapi")
-    gfpdir = os.path.join(outdir, "gfp")
-    # rufdir = os.path.join(outdir, "ruffles")
-    rfpdir = os.path.join(outdir, "rfp")
-    channelsdir = os.path.join(outdir, "channels")
-    if not os.path.isdir(dapidir):
-        os.mkdir(dapidir)
-    if not os.path.isdir(gfpdir):
-        os.mkdir(gfpdir)
-    # if not os.path.isdir(rufdir):
-    #     os.mkdir(rufdir)
-    if not os.path.isdir(rfpdir):
-        os.mkdir(rfpdir)
+    dapidir = os.path.join(outdir, "Channel1")
+    gfpdir = os.path.join(outdir, "Channel2")
+    rufdir = os.path.join(outdir, "Channel3")
+    rfpdir = os.path.join(outdir, "Channel4")
+    channelsdir = os.path.join(outdir, "Channels")
+    if not os.path.isdir(c1dir):
+        os.mkdir(c1dir)
+    if not os.path.isdir(c2dir):
+        os.mkdir(c2dir)
+    if not os.path.isdir(c3dir):
+        os.mkdir(c3dir)
+    if not os.path.isdir(c4dir):
+        os.mkdir(c4dir)
     if not os.path.isdir(channelsdir):
         os.mkdir(channelsdir)
 
     # Collect all file paths in the input directory
     files = readdirfiles(indir)
 
-    dapiResults = ResultsTable()
-    gfpResults = ResultsTable()
-    # rufResults = ResultsTable()
-    rfpResults = ResultsTable()
+    # Initialize the results tables.
+    c1Results = ResultsTable()
+    c2Results = ResultsTable()
+    c3Results = ResultsTable()
+    c4Results = ResultsTable()
 
     for file in files:
+
+        IJ.log("File: {}/{}".format(files.index(file)+1, len(files)))
+
         if file.endswith('ome.tif') or file.endswith('ome.tiff'):
-            imp = stackprocessor(file,
-                                   nChannels=3,
-                                   nSlices=7,
-                                   nFrames=1)
+
+            # Open .tiff file as ImagePlus.
+            imp = Opener().openImage(indir, imfile)
             channels = ChannelSplitter.split(imp)
             name = imp.getTitle()
-            IJ.log("Processing image: {}".format(name))
-            for c in range(len(channels)):
-                IJ.run(channels[c], "Grays", "")
-                IJ.run(channels[c], "Invert", "")
-                jpgname = channels[c].getShortTitle()
+            
+            # For every channel, save the inverted channel in grayscale as .jpg.
+            for channel in channels:
+                IJ.run(channel, "Grays", "")
+                IJ.run(channel, "Invert", "")
+                jpgname = channel.getShortTitle()
                 jpgoutfile = os.path.join(channelsdir, "{}.jpg".format(jpgname))
-                IJ.saveAs(channels[c].flatten(), "Jpeg", jpgoutfile)
-                IJ.run(channels[c], "Invert", "")
+                IJ.saveAs(channel.flatten(), "Jpeg", jpgoutfile)
+                IJ.run(channel, "Invert", "")
 
-            dapi = countobjects(channels[0], dapiResults,
+            # Settings for channel1 threshold.
+            c1 = countobjects(channels[0], c1Results,
                                threshMethod="Triangle",
                                subtractBackground=True,
-                               # dilate=True,
                                watershed=True,
                                minSize=3.00,
                                maxSize=100,
                                minCirc=0.00,
                                maxCirc=1.00)
 
-            gfp = countobjects(channels[1], gfpResults,
+            # Settings for channel2 threshold.
+            c2 = countobjects(channels[1], c2Results,
                                threshMethod="RenyiEntropy",
                                subtractBackground=False,
                                watershed=False,
@@ -152,14 +158,16 @@ def main():
                                minCirc=0.00,
                                maxCirc=1.00)
 
-            # ruf = countobjects(channels[2], rufResults,
-            #                    threshMethod="RenyiEntropy",
-            #                    minSize=2.00,
-            #                    maxSize=30.00,
-            #                    minCirc=0.20,
-            #                    maxCirc=1.00)
+            # Settings for channel3 threshold.
+            c3 = countobjects(channels[2], c3Results,
+                               threshMethod="RenyiEntropy",
+                               minSize=2.00,
+                               maxSize=30.00,
+                               minCirc=0.20,
+                               maxCirc=1.00)
 
-            rfp = countobjects(channels[2], rfpResults,
+            # Settings for channel4 threshold.
+            c4 = countobjects(channels[3], c4Results,
                                threshMethod="RenyiEntropy",
                                subtractBackground=False,
                                watershed=True,
@@ -168,30 +176,35 @@ def main():
                                minCirc=0.00,
                                maxCirc=1.00)
 
-            outfilenuc = os.path.join(dapidir, "threshold_nuc_{}".format(name))
-            outfilebac = os.path.join(gfpdir, "threshold_bac_{}".format(name))
-            # outfileruf = os.path.join(rufdir, "threshold_ruf_{}".format(name))
-            outfilegfp = os.path.join(rfpdir, "threshold_gfp_{}".format(name))
+            # Format filenames for thresholded .tiff files.
+            outfileC1 = os.path.join(c1dir, "threshold_c1_{}".format(name))
+            outfileC2 = os.path.join(c2dir, "threshold_c2_{}".format(name))
+            outfileC3 = os.path.join(c3dir, "threshold_c3_{}".format(name))
+            outfileC4 = os.path.join(c4dir, "threshold_c4_{}".format(name))
 
-            IJ.saveAs(dapi.flatten(), "Tiff", outfilenuc)
-            IJ.saveAs(gfp.flatten(), "Tiff", outfilebac)
-            # IJ.saveAs(ruf.flatten(), "Tiff", outfileruf)
-            IJ.saveAs(rfp.flatten(), "Tiff", outfilegfp)
+            # Save thresholded .tiff files.
+            IJ.saveAs(c1.flatten(), "Tiff", outfileC1)
+            IJ.saveAs(c2.flatten(), "Tiff", outfileC2)
+            IJ.saveAs(c3.flatten(), "Tiff", outfileC3)
+            IJ.saveAs(c4.flatten(), "Tiff", outfileC4)
 
-    dapiResults.show("dapi")
-    gfpResults.show("gfp")
-    # rufResults.show("ruffles")
-    rfpResults.show("rfp")
+    # Show results tables.
+    c1Results.show("channel1")
+    c2Results.show("channel2")
+    c3Results.show("channel3")
+    c4Results.show("channel4")
 
-    dapiout = os.path.join(outdir, "dapi.csv")
-    gfpout = os.path.join(outdir, "gfp.csv")
-    # rufout = os.path.join(outdir, "ruffles.csv")
-    rfpout = os.path.join(outdir, "rfp.csv")
+    # Prepare results table filenames.
+    c1out = os.path.join(outdir, "channel1.csv")
+    c2out = os.path.join(outdir, "channel2.csv")
+    c3out = os.path.join(outdir, "channel3.csv")
+    c4out = os.path.join(outdir, "channel4.csv")
 
-    ResultsTable.save(dapiResults, dapiout)
-    ResultsTable.save(gfpResults, gfpout)
-    # ResultsTable.save(rufResults, rufout)
-    ResultsTable.save(rfpResults, rfpout)
+    # Save results tables.
+    ResultsTable.save(c1Results, c1out)
+    ResultsTable.save(c2Results, c2out)
+    ResultsTable.save(c3Results, c3out)
+    ResultsTable.save(c4Results, c4out)
 
 
 main()
